d73d63845d79413ae6191d3cba049da85084088e
james sangho nah <sangho.nah@gmail.com>
2015-05-09 22:30:40 +1200

    custom powerline

--- elisp/microamp-mode-line.el
+++ elisp/microamp-mode-line.el
@@ -1,70 +1,67 @@
 (require 'powerline)
 
-(defun set-mode-line-format ()
+(defun powerline-custom-theme ()
+  "Set up your custom mode-line."
   (interactive)
   (setq-default mode-line-format
-                (list
-                 " "
-                 ;; the buffer name; the file name as a tool tip
-                 '(:eval (propertize "%b " 'face 'font-lock-keyword-face
-                                     'help-echo (buffer-file-name)))
-                 "| "
+                '("%e"
+                  (:eval
+                   (let* ((active (powerline-selected-window-active))
+                          (mode-line (if active 'mode-line 'mode-line-inactive))
+                          (face1 (if active 'powerline-active1 'powerline-inactive1))
+                          (face2 (if active 'powerline-active2 'powerline-inactive2))
+                          (separator-left (intern (format "powerline-%s-%s"
+							  (powerline-current-separator)
+                                                          (car powerline-default-separator-dir))))
+                          (separator-right (intern (format "powerline-%s-%s"
+                                                           (powerline-current-separator)
+                                                           (cdr powerline-default-separator-dir))))
+                          (lhs (list (powerline-raw "%*" nil 'l)
+                                     (when powerline-display-buffer-size
+                                       (powerline-buffer-size nil 'l))
+                                     (when powerline-display-mule-info
+                                       (powerline-raw mode-line-mule-info nil 'l))
+                                     (powerline-buffer-id nil 'l)
+                                     (when (and (boundp 'which-func-mode) which-func-mode)
+                                       (powerline-raw which-func-format nil 'l))
+                                     (powerline-raw " ")
+                                     (funcall separator-left mode-line face1)
+                                     (when (boundp 'erc-modified-channels-object)
+                                       (powerline-raw erc-modified-channels-object face1 'l))
+                                     (powerline-major-mode face1 'l)
+                                     (powerline-process face1)
+                                     (powerline-raw " ||" face1)
+                                     (powerline-minor-modes face1 'l)
+                                     (powerline-narrow face1 'l)
+                                     (powerline-raw " " face1)
+                                     (funcall separator-left face1 face2)
+                                     (powerline-vc face2 'r)
+                                     (when (bound-and-true-p nyan-mode)
+                                       (powerline-raw (list (nyan-create)) face2 'l))))
+                          (rhs (list (powerline-raw global-mode-string face2 'r)
+                                     (funcall separator-right face2 face1)
+				     (unless window-system
+				       (powerline-raw (char-to-string #xe0a1) face1 'l))
+				     (powerline-raw "%4l" face1 'l)
+				     (powerline-raw ":" face1 'l)
+				     (powerline-raw "%3c" face1 'r)
+				     (funcall separator-right face1 mode-line)
+				     (powerline-raw " ")
+				     (powerline-raw "%6p" nil 'r)
+                                     (when powerline-display-hud
+                                       (powerline-hud face2 face1)))))
+		     (concat (powerline-render lhs)
+			     (powerline-fill face2 (powerline-width rhs))
+			     (powerline-render rhs)))))))
 
-                 ;; line and column
-                 "(" ;; '%02' to set to 2 chars at least; prevents flickering
-                 (propertize "%02l" 'face 'font-lock-type-face) ","
-                 (propertize "%02c" 'face 'font-lock-type-face)
-                 ") "
-
-                 ;; relative position, size of file
-                 "["
-                 (propertize "%p" 'face 'font-lock-constant-face) ;; % above top
-                 "/"
-                 (propertize "%I" 'face 'font-lock-constant-face) ;; size
-                 "] "
-
-                 ;; the current major mode for the buffer.
-                 "["
-                 '(:eval (propertize "%m" 'face 'font-lock-string-face
-                                     'help-echo buffer-file-coding-system))
-                 "] "
-
-
-                 "[" ;; insert vs overwrite mode, input-method in a tooltip
-                 '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
-                                     'face 'font-lock-preprocessor-face
-                                     'help-echo (concat "Buffer is in "
-                                                        (if overwrite-mode "overwrite" "insert") " mode")))
-
-                 ;; was this buffer modified since the last save?
-                 '(:eval (when (buffer-modified-p)
-                           (concat "|"  (propertize "Mod"
-                                                    'face 'font-lock-warning-face
-                                                    'help-echo "Buffer has been modified"))))
-
-                 ;; is this buffer read-only?
-                 '(:eval (when buffer-read-only
-                           (concat "|"  (propertize "RO"
-                                                    'face 'font-lock-type-face
-                                                    'help-echo "Buffer is read-only"))))
-                 "] "
-
-                 "{"
-                 '(vc-mode vc-mode)
-                 " } "
-
-                 ;; i don't want to see minor-modes; but if you want, uncomment this:
-                 ;minor-mode-alist
-
-                 "%-" ;; fill with '-'
-                 )))
+(set-face-foreground 'powerline-active1 "#a89984")
+(set-face-foreground 'powerline-active2 "#fe8019")
 
 (defun set-powerline-format ()
   (interactive)
-  (powerline-default-theme))
+  (powerline-custom-theme)
+  (setq powerline-default-separator 'wave))
 
 (set-powerline-format)
-
-(setq powerline-default-separator 'wave)
 
 (provide 'microamp-mode-line)
